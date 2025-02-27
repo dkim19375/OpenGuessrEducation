a149c31ec4b9a55463dbee4cb1e884e994a2187e
dkim19375 <65050767+dkim19375@users.noreply.github.com>
2025-02-26 20:22:27 -0500

    Underline

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,8 +1,8 @@
 <footer class="footer footer-center bg-base-200 text-base-content p-3">
     <aside>
         <p>
-            Credits to <a href="https://education.openguessr.com/quiz/play/countries/africa"
-                          target="_blank">OpenGuessr</a>!
+            Credits to <u><a href="https://education.openguessr.com/quiz/play/countries/africa"
+                             target="_blank">OpenGuessr</a></u>!
         </p>
     </aside>
 </footer>
